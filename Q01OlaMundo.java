2be3028901ab78e242d575923c7c18600614f684
KevinArrudaDev <kevin.arruda@proton.me>
2026-04-13 14:45:36 -0300

    Add files via upload

---
+++ Q01OlaMundo.java
@@ -0,0 +1,10 @@
+/* Questão 1) Escreva um programa que mostre na tela 
+a mensagem "Olá, Mundo!" */
+
+public class Q01OlaMundo {
+    public static void main(String[] args) {
+        
+        System.out.println("Olá, mundo!");
+    }
+    
+}
